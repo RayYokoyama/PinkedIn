1ab6bb9a6b23d21f3b4d48cbdb52d8b3ef1c17ca
ray_yokoyama <kingreibit@gmail.com>
2019-12-10 21:54:40 +0900

    ログイン実装, name, hiraganaの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  root to: 'home#index'
+  devise_for :users, controllers: {
+    confirmations: 'users/confirmations',
+    passwords:     'users/passwords',
+    registrations: 'users/registrations',
+    sessions:      'users/sessions',
+  }
 end
