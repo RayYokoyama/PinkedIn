3cd1faf77515f1faf73d3c9ca1f40e7560677935
ray_yokoyama <kingreibit@gmail.com>
2019-12-11 21:21:32 +0900

    ci で yarn するように修正

--- app/models/enterprise_account.rb
+++ app/models/enterprise_account.rb
@@ -1,2 +1,6 @@
 class EnterpriseAccount < ApplicationRecord
+  validates :name, presence: true
+  validates :email, presence: true
+
+  has_many :users, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+    :recoverable, :rememberable, :validatable
+
   validates :name, presence: true
   validates :email, presence: true
   validates :hiragana, presence: true
 
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  belongs_to :enterprise_account
 end
