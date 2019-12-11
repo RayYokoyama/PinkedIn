e2f59927390e66c024c1e166439ec7e809668e4d
ray_yokoyama <kingreibit@gmail.com>
2019-12-11 19:59:32 +0900

    headerの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
   root to: 'home#index'
-  devise_for :users, controllers: {
-    confirmations: 'users/confirmations',
-    passwords:     'users/passwords',
-    registrations: 'users/registrations',
-    sessions:      'users/sessions',
-  }
+  devise_for :users,
+    path: '',
+    path_names: {
+      sign_up: '',
+      sign_in: 'login',
+      sign_out: 'logout',
+      registration: "signup",
+    },
+    controllers: {
+      confirmations: 'users/confirmations',
+      passwords:     'users/passwords',
+      registrations: 'users/registrations',
+      sessions:      'users/sessions',
+    }
 end
