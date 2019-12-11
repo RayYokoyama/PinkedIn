0a457a5070382c23e7c9fc0d9d2b7ae54da09e22
ray_yokoyama <kingreibit@gmail.com>
2019-12-11 20:48:41 +0900

    spec

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe HomeController, type: :controller do
 
   describe "GET #index" do
     it "returns http success" do
-      get :index
+      get root_path
       expect(response).to have_http_status(:success)
     end
   end
