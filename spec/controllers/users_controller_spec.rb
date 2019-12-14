e7210f258768dbb3e4aa08e892b0da5de419c8f2
ray_yokoyama <kingreibit@gmail.com>
2019-12-14 16:44:55 +0900

    add devise login to spec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,7 +3,11 @@ require 'rails_helper'
 RSpec.describe UsersController, type: :controller do
 
   describe "GET #show" do
-    before { get :show, params: {}, session:{} }
+    let(:user) { create(:normal_user) }
+    before {
+      login_user user
+      get :show, params: {}, session:{}
+    }
     it "returns http success" do
       expect(response).to have_http_status(:success)
     end
