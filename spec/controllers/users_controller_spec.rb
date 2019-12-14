1639d2a8d9f1de2ecc09d85b6f8fc641a03a84c4
ray_yokoyama <kingreibit@gmail.com>
2019-12-14 16:21:39 +0900

    add coverage

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe UsersController, type: :controller do
 
   describe "GET #show" do
+    before { get :show, params: {}, session:{} }
     it "returns http success" do
-      get :show
       expect(response).to have_http_status(:success)
     end
   end
