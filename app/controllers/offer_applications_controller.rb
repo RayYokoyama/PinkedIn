6e327b1de03eaab4115c55b69a32fc0b03ad6ad2
ray_yokoyama <kingreibit@gmail.com>
2019-12-31 00:06:03 +0900

    応募の実装

--- app/controllers/offer_applications_controller.rb
+++ app/controllers/offer_applications_controller.rb
@@ -1,6 +1,8 @@
 class OfferApplicationsController < ApplicationController
   def create
-
+    @offer = Offer.find(params[:offer_application][:offer_id])
+    current_user.apply_offer!(@offer)
+    redirect_to offer_path(@offer)
   end
 
   def update
--- spec/controllers/offer_applications_controller_spec.rb
+++ spec/controllers/offer_applications_controller_spec.rb
@@ -1,19 +1,48 @@
 require 'rails_helper'
 
 RSpec.describe OfferApplicationsController, type: :controller do
+  let(:user) { create(:normal_user) }
+  before { login user }
+  describe "POST #create" do
+    let(:offer) { create(:offer) }
+    let(:params) { {
+      offer_application: {
+        offer_id: offer.id 
+      }
+    } }
 
-  # describe "GET #create" do
-  #   it "returns http success" do
-  #     get :create
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
-  #
-  # describe "GET #update" do
-  #   it "returns http success" do
-  #     get :update
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+    it "return http success" do
+      expect(response).to have_http_status(:success)
+    end
 
+    it "offer_aplicationが作成される" do
+      expect do
+        post :create, params: params
+      end.to change(OfferApplication, :count).by(1)
+    end
+  end
+  
+  describe "PUT #update" do
+    let(:update_params) { {
+      id: offer.id,
+      offer: {
+        name: "test_offer_#{Time.zone.now.to_i}",
+        fee_type: :month,
+        fee: 100000,
+        description: 'this is test offer',
+        deadline: Time.zone.now
+      }
+    } }
+
+    it "return http success" do
+      expect(response).to have_http_status(:success)
+    end
+
+    it "求人が更新されている" do
+      offer.enterprise_account = user.enterprise_account
+      put :update, params: update_params
+      offer.reload
+      expect(offer.name).to eq update_params[:offer][:name]
+    end
+  end
 end
