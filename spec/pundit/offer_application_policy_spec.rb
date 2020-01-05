9e833f39e6a76b98f67ce2cf25826b07e97334d4
ray_yokoyama <kingreibit@gmail.com>
2020-01-05 16:57:00 +0900

    add offer_application_policy

---
+++ spec/pundit/offer_application_policy_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+describe OfferApplicationPolicy do
+  let(:offer_application) { create(:offer_application) }
+  subject { described_class.new(user, offer_application) }
+
+  context '未ログインの時' do
+    let(:user) { nil }
+  
+    it { is_expected.to_not authorize(:create)  }
+    it { is_expected.to_not authorize(:update)  }
+    it { is_expected.to_not authorize(:destroy) }
+  end
+  
+  context '一般ユーザーの場合' do
+    let(:user) { create(:normal_user) }
+  
+    it { is_expected.to     authorize(:create)  }
+    it { is_expected.to_not authorize(:update)  }
+    it { is_expected.to_not authorize(:destroy) }
+  end
+  
+  context '企業アカウントユーザーの場合' do
+    let(:user) { create(:ea_user) }
+    before {
+      offer_application.user = user
+      offer_application.save
+    }
+  
+    it { is_expected.to_not authorize(:create)  }
+    it { is_expected.to     authorize(:update)  }
+    it { is_expected.to     authorize(:destroy) }
+  end
+  
+  context 'adminユーザーの場合' do
+    let(:user) { create(:admin_user) }
+  
+    it { is_expected.to authorize(:create)  }
+    it { is_expected.to authorize(:update)  }
+    it { is_expected.to authorize(:destroy) }
+  end
+end
